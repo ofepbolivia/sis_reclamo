025a4db934acea0160cea1db9a42cbc7d3ced55d
franklin <franklin.espinoza.alvarez@gmail.com>
2016-08-26 16:32:40 -0400

    inserts cliente y incidentes

--- base/data000001.sql
+++ base/data000001.sql
@@ -80,3 +80,29 @@ select pxp.f_insert_testructura_gui ('RTI', 'REC');
 select pxp.f_insert_tgui ('Reclamo', 'Reclamo', 'RECM', 'si', 1, 'sis_reclamo/vista/reclamo/Reclamo.php', 2, '', 'Reclamo', 'REC');
 select pxp.f_insert_testructura_gui ('RECM', 'REC');
 /***********************************F-DAT-MAM-REC-2-23/08/2016****************************************/
+
+/***********************************I-DAT-FEA-REC-1-26/08/2016****************************************/
+INSERT INTO rec.tcliente ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_cliente", "ci", "lugar_expedicion", "nombre", "apellido_paterno", "apellido_materno", "genero", "nacionalidad", "email", "celular", "telefono", "pais_residencia", "ciudad_residencia", "barrio_zona", "direccion")
+VALUES
+  (1, 1, E'2016-08-12 11:53:54.216', E'2016-08-15 12:19:30.336', E'activo', NULL, E'NULL', 1, E'prueba', E'prueba', E'Grover', E'prueba', E'prueba', E'h', E'prueba', E'prueba', 1, 1, E'prueba', E'prueba', E'prueba', E'prueba'),
+  (1, 1, E'2016-08-12 11:53:59.700', E'2016-08-22 10:17:43.410', E'activo', NULL, E'NULL', 2, E'prueba', E'prueba', E'Franklin', E'prueba', E'prueba', E'm', E'prueba', E'prueba', 1, 1, E'prueba', E'prueba', E'prueba', E'prueba'),
+  (1, 1, E'2016-08-22 11:31:07.069', E'2016-08-26 11:27:42.693', E'activo', NULL, E'NULL', 3, E'4564654', E'SC', E'federico', E'fernandez', E'mendez', E'VARON', E'boliviano', E'federico@gmail.com', 78546546, 4564655, E'bolivia', E'Santa cruz', E'las cuadras', E'calle ballivian # 156'),
+  (1, 1, E'2016-08-22 12:15:41.848', E'2016-08-26 11:21:43.716', E'activo', NULL, E'NULL', 4, E'4845465', E'SC', E'PEDRO', E'JIMENEZ', E'PEREZ', E'varon', E'Boliviano', E'pedro@gmail.com', 78456544, 4565656, E'bolivia', E'cochabamba', E'cercado', E'Calle bolivar'),
+  (1, 1, E'2016-08-22 12:19:54.270', E'2016-08-26 11:29:38.235', E'activo', NULL, E'NULL', 5, E'89894754', E'BN', E'carla', E'rojas', E'cruz', E'MUJER', E'Boliviana', E'carla@gmail.com', 75646546, 3546215, E'Bolivia', E'Tarija', E'Morados', E'Calle  la Esquina'),
+  (1, 1, E'2016-08-22 12:22:04.100', E'2016-08-26 11:24:03.190', E'activo', NULL, E'NULL', 6, E'4324334', E'TJ', E'carlos', E'alvarez', E'zeballos', E'VARON', E'ccccc', E'sdfs@hotmail.com', 5456465, 4564, E'CHILE', E'LA PAZ', E'MAYORAZGO', E'Av. Ayacucho #456'),
+  (1, NULL, E'2016-08-22 12:40:59.068', NULL, E'activo', NULL, E'NULL', 7, E'45656566', E'CB', E'VANESSA', E'ZEBALLOS', E'CORDOVA', E'MUJER', E'BOLIVIANA', E'zeballos@gmail.com', 72275654, 4564564, E'bolivia', E'cochabamba', E'el jardin', E'calle jordan esquina nataniel aguirre.'),
+  (1, 1, E'2016-08-23 16:25:58.699', E'2016-08-26 11:31:06.647', E'activo', NULL, E'NULL', 8, E'4566456', E'CB', E'martha', E'meneces', E'espinoza', E'MUJER', E'BolivianA', E'marta@gmail.com', 78465646, 4565656, E'bolivia', E'cochabamba', E'quillacollo', E'calle oquendo # 56'),
+  (1, 1, E'2016-08-23 16:44:02', E'2016-08-26 11:23:42.132', E'activo', NULL, E'NULL', 9, E'4568946', E'CB', E'adriana', E'adriazola', E'enriquez', E'MUJER', E'boliviana', E'adriana@gmail.com', 75462145, 4621312, E'bolivia', E'cochabamba', E'cercado', E'calle sucre esq. san martin');
+
+INSERT INTO rec.ttipo_incidente ("id_usuario_reg", "id_usuario_mod", "fecha_reg", "fecha_mod", "estado_reg", "id_usuario_ai", "usuario_ai", "id_tipo_incidente", "nombre_incidente", "fk_tipo_incidente", "tiempo_respuesta", "nivel")
+VALUES
+  (1, NULL, E'2016-08-25 18:29:08.451', NULL, E'activo', NULL, E'NULL', 1, E'Reclamos', NULL, 7, 0),
+  (1, NULL, E'2016-08-25 18:29:34.876', NULL, E'activo', NULL, E'NULL', 2, E'Solicitud', NULL, 5, 0),
+  (1, NULL, E'2016-08-25 18:29:51.360', NULL, E'activo', NULL, E'NULL', 3, E'Comentario', NULL, 5, 0),
+  (1, 1, E'2016-08-25 18:31:39.416', E'2016-08-26 10:47:48.302', E'activo', NULL, E'NULL', 4, E'Equipaje', 1, 10, 1),
+  (1, NULL, E'2016-08-25 18:32:13.916', NULL, E'activo', NULL, E'NULL', 6, E'Carga/Encomienda', 1, 10, 1),
+  (1, NULL, E'2016-08-26 10:47:19.913', NULL, E'activo', NULL, E'NULL', 36, E'Vuelo', 1, 7, 1),
+  (1, NULL, E'2016-08-26 10:49:52.246', NULL, E'activo', NULL, E'NULL', 37, E'Pasaje/Boleto', 1, 10, 1),
+  (1, NULL, E'2016-08-26 10:51:05.468', NULL, E'activo', NULL, E'NULL', 38, E'Atencion al Usuario', 1, 10, 1),
+  (1, NULL, E'2016-08-26 12:25:06.273', NULL, E'activo', NULL, E'NULL', 39, E'jkhjk', 38, 4, 2);
+/***********************************F-DAT-FEA-REC-1-26/08/2016****************************************/
