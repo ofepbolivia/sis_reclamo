65e5299cb0310c60406c60d930d0404db0d6a98c
franklinespinozaalvarez <franklin.espinoza.alvarez@gmail.com>
2017-09-26 17:49:19 -0400

    Update VoBoRespuesta

--- vista/respuesta/VoBoRespuesta.php
+++ vista/respuesta/VoBoRespuesta.php
@@ -14,27 +14,8 @@ header("content-type: text/javascript; charset=UTF-8");
         requireclase:'Phx.vista.Respuesta',
         title:'Respuesta',
         nombreVista: 'VoBoRespuesta',
-        //layoutType: 'wizard',
         bnew:false,
         bdel:false,
-        /*gruposBarraTareas:[
-
-            {name:'revision_legal',title:'<H1 align="center"><i class="fa fa-legal"></i> Revision Legal</h1>',grupo:0,height:0},
-            {name:'vobo_respuesta',title:'<H1 align="center"><i class="fa fa-thumbs-o-up"></i>VoBo. Respuesta</h1>',grupo:1,height:0}
-
-        ],
-
-        actualizarSegunTab: function(name, indice){
-            if(this.finCons){
-                this.store.baseParams.pes_estado = name;
-                this.load({params:{start:0, limit:this.tam_pag}});
-            }
-        },
-        beditGroups: [0,1],
-        bdelGroups:  [0,1],
-        bactGroups:  [0,1],
-        btestGroups: [],
-        bexcelGroups: [0,1],*/
         tam_pag: 50,
         constructor: function(config) {
             this.maestro = config.maestro;
@@ -44,17 +25,12 @@ header("content-type: text/javascript; charset=UTF-8");
             this.store.baseParams.pes_estado = 'vobo_respuesta';
             this.load({params:{start:0, limit:this.tam_pag}});
             this.finCons = true;
-
-
         },
 
         preparaMenu:function(n){
             var data = this.getSelectedData();
             var tb =this.tbar;
             Phx.vista.VoBoRespuesta.superclass.preparaMenu.call(this,n);
-
-            //this.getBoton('sig_estado').enable();
-            //this.getBoton('ant_estado').enable();
 
             if(data.estado =='vobo_respuesta'){
                 this.getBoton('sig_estado').enable();
@@ -65,33 +41,6 @@ header("content-type: text/javascript; charset=UTF-8");
                 console.log('DOS');
             }
 
-            /*Ext.Ajax.request({
-                url:'../../sis_reclamo/control/Reclamo/getDatosOficina',
-                params:{id_usuario: 0},
-                success:function(resp){
-                    var reg = Ext.util.JSON.decode(Ext.util.Format.trim(resp.responseText));
-                    console.log('datos: '+JSON.stringify(reg.ROOT.datos));
-                    //&& reg.ROOT.datos.nombre_cargo == 'Responsable Atención al Cliente'
-                    if(data.estado =='vobo_respuesta'){
-                        this.getBoton('sig_estado').enable();
-                        this.getBoton('ant_estado').enable();
-                        this.getBoton('diagrama_gantt').enable();
-                        this.getBoton('btnObs').enable();
-                        this.getBoton('btnChequeoDocumentosWf').enable();
-                        console.log('DOS');
-                    }else{
-                        this.getBoton('sig_estado').disable();
-                        this.getBoton('ant_estado').disable();
-                        this.getBoton('diagrama_gantt').disable();
-                        this.getBoton('btnObs').disable();
-                        this.getBoton('btnChequeoDocumentosWf').disable();
-                        console.log('TRES');
-                    }
-                },
-                failure: this.conexionFailure,
-                timeout:this.timeout,
-                scope:this
-            });*/
             return tb
         },
         liberaMenu:function(){
